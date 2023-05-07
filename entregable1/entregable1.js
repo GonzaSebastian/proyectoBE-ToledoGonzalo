a720b6b10c73c0bacbb1980d92f7c193c1cd48ff
GonzaSebastian <gonzalo.stoledo@gmail.com>
2023-05-07 16:52:10 -0300

    se crea class productManager + constructor

---
+++ entregable1/entregable1.js
@@ -0,0 +1,13 @@
+class productManager {
+  products = [];
+
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.id = 
+    this.title = title
+    this.description = description
+    this.price = price
+    this.thumbnail = thumbnail
+    this.code = code
+    this.stock = stock
+  }
+}
