f0ba3d978dd3e1260d5aa9f644db2f58864b0368
GonzaSebastian <gonzalo.stoledo@gmail.com>
2023-05-07 23:41:05 -0300

    se corrige contructor + se crean metodos addProduct y getProducts

--- entregable1/entregable1.js
+++ entregable1/entregable1.js
@@ -1,13 +1,21 @@
 class productManager {
-  products = [];
-
-  constructor(title, description, price, thumbnail, code, stock) {
-    this.id = 
-    this.title = title
-    this.description = description
-    this.price = price
-    this.thumbnail = thumbnail
-    this.code = code
-    this.stock = stock
+  #products
+  constructor() {
+    this.#products = []
   }
-}
+
+  getProducts = () => this.#products
+
+  addProduct = (title, description, price, thumbnail, code, stock) => {
+    this.#products.push({title, description, price, thumbnail, code, stock})
+  }
+} 
+
+const product = new productManager()
+
+product.addProduct('PGC 100', 'Perfil estructural C 100mm', 4500, './notImage', 'c100', 98)
+// product.addProduct('PGU 100', 'Perfil estructural U 100mm', 3500, './notImage', 'u100', 45)
+
+
+console.log(product.getProducts());
+console.log(product.products);
