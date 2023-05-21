a9043464f82d1baa820b784e91198ae50cab4ede
Gonzalo Toledo <gonzalo.stoledo@gmail.com>
2023-05-21 20:32:35 -0300

    se crea route products/:pid con url params limit

--- entregable3/src/app.js
+++ entregable3/src/app.js
@@ -7,15 +7,26 @@ const product = new productManager("./products.json")
 
 const getProducts = await product.getProducts()
 
-app.get('/products', (request, response) => {
-  let limit = parseInt(request.query.limit)
+app.get('/products', (req, res) => {
+  let limit = parseInt(req.query.limit)
 
   if (!limit) {
-    response.send({getProducts})
+    res.send(getProducts)
   } else {
     const productsLimit = getProducts.slice(0, limit)
-    response.send(productsLimit)
+    res.send(productsLimit)
   }
+
+})
+
+app.get('/products/:pid', (req, res) => {
+  const id = parseInt(req.params.pid)
+  const product = getProducts.find(i => i.id === id)
+
+
+  if (!product) return res.send({ error: 'El producto no existe' })
+    res.send(product)
+  
 })
 
 app.listen(8080, () => console.log('Server Up...'))
